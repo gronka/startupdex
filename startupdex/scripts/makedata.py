73f2b3c8ba139b8018df6ee416a085c56627b8a9
gronka <mr.gronka@gmail.com>
2015-05-19 16:58:10 -0400

    commenting sqlite

--- startupdex/scripts/makedata.py
+++ startupdex/scripts/makedata.py
@@ -1,22 +1,22 @@
-import sys
-from peewee import *
-from playhouse.sqlite_ext import *
-from ..models import Entry, FTSEntry
+#import sys
+#from peewee import *
+#from playhouse.sqlite_ext import *
+#from ..models import Entry, FTSEntry
 
-def main(argv=sys.argv):
-	entry = Entry.create(
-		title="How I rewrote everything in golang",
-		content="blah blah blah golang is awesome",
-	)
-	FTSEntry.create(
-		entry=entry,
-		content='\n'.join((entry.title, entry.content))
-	)
-	entry = Entry.create(
-		title="Here is another article about golang",
-		content="after much consideration, I no longer think golang is awesome",
-	)
-	FTSEntry.create(
-		entry=entry,
-		content='\n'.join((entry.title, entry.content))
-	)
+#def main(argv=sys.argv):
+	#entry = Entry.create(
+		#title="How I rewrote everything in golang",
+		#content="blah blah blah golang is awesome",
+	#)
+	#FTSEntry.create(
+		#entry=entry,
+		#content='\n'.join((entry.title, entry.content))
+	#)
+	#entry = Entry.create(
+		#title="Here is another article about golang",
+		#content="after much consideration, I no longer think golang is awesome",
+	#)
+	#FTSEntry.create(
+		#entry=entry,
+		#content='\n'.join((entry.title, entry.content))
+	#)
